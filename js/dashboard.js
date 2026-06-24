6fd4850c10ac2565bb36c6424922a862006b63ce
Cognicion <aldousaquila@gmail.com>
2026-06-24 17:57:44 -0600

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,1 +1,36 @@
+import { auth, db } from "./firebase.js";
 
+import {
+  onAuthStateChanged,
+  signOut
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  doc,
+  getDoc
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const refUsuario = doc(db, "usuarios", user.uid);
+  const snap = await getDoc(refUsuario);
+
+  if (snap.exists()) {
+    const datos = snap.data();
+
+    document.getElementById("bienvenida").innerText =
+      "Bienvenido, " + datos.nombre;
+  } else {
+    document.getElementById("bienvenida").innerText =
+      "Bienvenido";
+  }
+});
+
+window.cerrarSesion = async function() {
+  await signOut(auth);
+  window.location.href = "login.html";
+};
