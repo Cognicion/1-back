e78b011b16a863c1eb50fbe692d777087b71dd18
Cognicion <aldousaquila@gmail.com>
2026-06-24 18:15:52 -0600

    Update notas.js

--- js/services/notas.js
+++ js/services/notas.js
@@ -1,1 +1,28 @@
+import { db } from "../firebase.js";
 
+import {
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  orderBy
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+export async function guardarNota(uidPaciente, datosNota) {
+  await addDoc(
+    collection(db, "usuarios", uidPaciente, "notasMedicas"),
+    {
+      ...datosNota,
+      fecha: new Date().toISOString()
+    }
+  );
+}
+
+export async function obtenerHistorialNotas(uidPaciente) {
+  const q = query(
+    collection(db, "usuarios", uidPaciente, "notasMedicas"),
+    orderBy("fecha", "desc")
+  );
+
+  return await getDocs(q);
+}
