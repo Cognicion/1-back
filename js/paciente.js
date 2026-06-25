84ea26376c2c6780275d4ce53c6157740e6833b1
Cognicion <aldousaquila@gmail.com>
2026-06-24 18:12:13 -0600

    Update paciente.js

--- js/paciente.js
+++ js/paciente.js
@@ -1,1 +1,40 @@
+import { auth } from "./firebase.js";
 
+import {
+  onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+import {
+  obtenerUsuario
+} from "./services/usuarios.js";
+
+onAuthStateChanged(auth, async (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+    return;
+  }
+
+  const parametros = new URLSearchParams(window.location.search);
+  const uidPaciente = parametros.get("id");
+
+  const datos = await obtenerUsuario(uidPaciente);
+
+  if (!datos) {
+    document.getElementById("nombrePaciente").innerText =
+      "Paciente no encontrado";
+    return;
+  }
+
+  document.getElementById("nombrePaciente").innerText =
+    datos.nombre || "Paciente sin nombre";
+
+  document.getElementById("diagnostico").innerText =
+    "Diagnóstico: " + (datos.diagnostico || "Sin diagnóstico registrado");
+
+  document.getElementById("tratamiento").innerText =
+    "Tratamiento: " + (datos.tratamiento || "Sin tratamiento registrado");
+
+  window.abrirNota = function() {
+    window.location.href = "nota.html?id=" + uidPaciente;
+  };
+});
