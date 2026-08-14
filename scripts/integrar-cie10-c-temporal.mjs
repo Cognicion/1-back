5b61bcb74e422b90fd3d808144a88ef49528af44
Dr. Aldo Sandokan Aguilar Valenzuela <aldo.sandokan@outlook.com>
2026-08-13 21:57:50 -0600

    sad

--- scripts/integrar-cie10-c-temporal.mjs
+++
@@ -1,6 +0,0 @@
-import { createHash } from "node:crypto";
-import { readFile, writeFile } from "node:fs/promises";
-import { resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-import pdfplumber from "pdfplumber";
-
