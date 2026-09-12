ee5c6c75803de45e0d0c139377ca3bd3b8177c79
Dr. Aldo Sandokan Aguilar Valenzuela <aldo.sandokan@outlook.com>
2026-09-12 15:43:43 -0700

    adsad

--- js/config/appVersion.js
+++ js/config/appVersion.js
@@ -199,5 +199,6 @@
 // deployment marker: 2026-09-12-public-professional-directory-v1 (+0.001)
 // deployment marker: 2026-09-12-organizacion-estrategia-admin-v1 (+0.001)
 // deployment marker: 2026-09-12-index-public-care-callout-v1 (+0.001)
-export const APP_VERSION = "2.213";
+// deployment marker: 2026-09-12-index-editorial-open-layout-v1 (+0.001)
+export const APP_VERSION = "2.214";
 export const HISTORIA_CLINICA_EXPORT_VERSION = APP_VERSION;
